6fef7a2bd1fa27443a79dc70f4157aabe1cf9835
fatma gülfidan <nesepalamutlari@icloud.com>
2025-01-12 21:20:29 +0100

    Add AWS secret key for testing Secret Scanning

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import os
 API_KEY = "sk_test_51Lx1234567890ABCDEFGHIJKLMNOPQRSTU"
 
 print("Bu bir secret scanning testidir!")
+
+
+import os
+
+# Örnek bir AWS anahtarı (TEST AMAÇLI)
+AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+
+print("Bu bir secret scanning testidir!")
